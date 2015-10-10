e2e1b9b2e7b62ad6625dfe9851104ac9e45e0a15
Balint Szebenyi <szebenyib@gmail.com>
2015-10-10 14:45:09 +0200

    05_controllers

---
+++ sapui5/public_html/05_controllers/controller/App.controller.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+	return Controller.extend("namespace_id.controller.App", {
+		onShowHello: function() {
+			alert("Hello from controller!");
+		}
+	});
+});
